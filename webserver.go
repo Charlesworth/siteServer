794a960703a30851ad9aac8dfa23336f3c9bb443
charles cochrane <cochrane26@gmail.com>
2015-05-15 14:31:45 +0100

    added ParseGlob with multiple templates

--- webserver.go
+++ webserver.go
@@ -19,7 +19,7 @@ func main() {
 
 	router := httprouter.New()
 
-	router.GET("/", getTmpl)
+	router.GET("/index", testGlob)
 
 	http.Handle("/", router)
 
@@ -27,20 +27,27 @@ func main() {
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
 
-func get(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-
-	fmt.Fprint(w, "test")
-}
-
-func getTmpl(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+func getTest(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 
 	mock := PageViews{10, 20, 30}
 
-	tmpl := template.Must(template.ParseFiles("tmpl/1.html"))
+	tmpl := template.Must(template.ParseFiles("tmpl/comments.html", "tmpl/foot.html")) //"tmpl/head.html", "tmpl/post.html", "tmpl/comments.html", "tmpl/foot.html"))
 
 	err := tmpl.Execute(w, mock)
 	if err != nil {
 		fmt.Println(err)
+	}
+
+}
+
+func testGlob(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+
+	templates := template.Must(template.ParseGlob("tmpl/*"))
+
+	err := templates.ExecuteTemplate(w, "indexPage", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 }
